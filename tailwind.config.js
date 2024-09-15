0f6e0d3211a167ec7c5c8a25f0aae52aa4fcb770
lutiousXt54 <abhradeepdebnath@gmail.com>
2024-09-15 12:40:00 +0530

    tailwindcss file issu solved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}","./public/**/*.{html}"],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       screens:{
         "x-sm":{
           "min":"0px",
-          "max":"480px"
+          "max":"490px"
         }
       }
     },
